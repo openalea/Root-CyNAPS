3f949c9840d080f33bcfe0b4803d989fcbba4e3f
Tristan Gérault <120411182+MTreestanG@users.noreply.github.com>
2023-04-16 19:45:20 +0200

    logging to xarray

--- example/main.py
+++ example/main.py
@@ -2,7 +2,7 @@ from rhizodep.simulation_no_C import N_simulation
 
 
 def main(init, n, time_step):
-    N_simulation(init=init, n=n, time_step=time_step, discrete_vessels=True, plotting=False)
+    N_simulation(init=init, n=n, time_step=time_step, discrete_vessels=True, plotting=False, logging=True)
 
 
-main(init='inputs/root00478.pckl', n=60, time_step=3600)
+main(init='inputs/root00020.pckl', n=10, time_step=3600)
