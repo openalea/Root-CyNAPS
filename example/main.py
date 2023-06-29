d06c0ba4b46b624746912803b974eaf5b810e124
Tristan Gérault <120411182+MTreestanG@users.noreply.github.com>
2023-06-29 11:35:33 +0200

    model-specific sub_time_step tests

--- example/main.py
+++ example/main.py
@@ -4,8 +4,8 @@ from rhydromin.simulation_no_C import N_simulation
 
 
 def main(init, n, time_step):
-    N_simulation(init=init, n=n, time_step=time_step, discrete_vessels=True, plantgl=False, plotting_2D=False, plotting_STM=True, logging=True)
+    N_simulation(init=init, n=n, time_step=time_step, discrete_vessels=True, plantgl=False, plotting_2D=True, plotting_STM=False, logging=True)
 
 # This step is essential to ensure a systematic reference of subdirectories, at least for windows paths
 os.chdir(os.getcwd()[:os.getcwd().find("example")-1])
-main(init='example/inputs/root00020.pckl', n=240, time_step=3600)
+main(init='example/inputs/root00020.pckl', n=72, time_step=3600)
