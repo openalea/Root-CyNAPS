e1d6b2eb6a0bc6389546aca51f0cb07edf0c67c9
Tristan Gérault <tristan.gerault@inrae.fr>
2023-09-08 10:30:31 +0200

    moved stm analysis to tools

---
+++ tools/STM_analysis.py
@@ -0,0 +1,8 @@
+from STM_statistics.analysis import main_workflow
+
+def run(path):
+    """
+    :param path: specify path to xarray netcdf output files from mtg logging
+    :return:
+    """
+    main_workflow.run_analysis(path, EPOCHS=10, min_cluster_size=5500)
