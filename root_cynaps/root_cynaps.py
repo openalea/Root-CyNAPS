d1e0f5d1ce3517c32f14dcb4b47b8ca3e1540929
GERAULT Tristan <tristan.gerault@inrae.fr>
2024-06-08 11:54:46 +0200

    updated simulation

--- root_cynaps/root_cynaps.py
+++ root_cynaps/root_cynaps.py
@@ -1,11 +1,12 @@
 import root_cynaps
+
 from root_cynaps.root_nitrogen import RootNitrogenModel
 from root_cynaps.root_water import RootWaterModel
-from rhizodep.soil_model import SoilModel
+from rhizodep.soil_model import RhizoInputsSoilModel
 from rhizodep.root_anatomy import RootAnatomy
-from Data_enforcer.shoot import ShootModel
 
-from genericmodel.composite_wrapper import CompositeModel
+from metafspm.composite_wrapper import CompositeModel
+from metafspm.component_factory import Choregrapher
 
 
 class Model(CompositeModel):
@@ -32,23 +33,42 @@ class Model(CompositeModel):
         :param time_step: the resolution time_step of the model in seconds.
         """
 
+        # DECLARE GLOBAL SIMULATION TIME STEP
+        Choregrapher().add_simulation_time_step(time_step)
+        self.time = 0
+        parameters = scenario["parameters"]["root_cynaps"]
+        self.input_tables = scenario["input_tables"]
+
         # INIT INDIVIDUAL MODULES
         # NOTE : IT HAS TO BE DONE THROUGH THE LOAD FUNCTION TO ENSURE SEPARATED CHOREGRAPHER INSTANCE
-        self.g = g
-        self.soil = self.load(SoilModel, self.g, time_step, **scenario)
-        self.root_anatomy = self.load(RootAnatomy, self.g, time_step, **scenario)
-        self.root_water = self.load(RootWaterModel, self.g, time_step, **scenario)
-        self.root_nitrogen = self.load(RootNitrogenModel, self.g, time_step, **scenario)
-        self.shoot = ShootModel(self.g)
+        self.g = scenario["input_mtg"]
+        self.root_anatomy = RootAnatomy(self.g, time_step, **parameters)
+        self.root_water = RootWaterModel(self.g, time_step/10, **parameters)
+        self.root_nitrogen = RootNitrogenModel(self.g, time_step, **parameters)
+        self.soil = RhizoInputsSoilModel(self.g, time_step, **parameters)
+        self.soil_voxels = self.soil.voxels
 
         # ORDER MATTERS HERE !
-        self.models = (self.soil, self.shoot, self.root_water, self.root_nitrogen, self.root_anatomy)
+        self.models = (self.soil, self.root_water, self.root_nitrogen, self.root_anatomy)
+        self.data_structures = {"root": self.g, "soil": self.soil_voxels}
 
         # LINKING MODULES
         self.link_around_mtg(translator_path=root_cynaps.__path__[0])
 
         # Some initialization must be performed after linking modules
-        [m.post_coupling_init() for m in self.models]
+        self.root_water.post_coupling_init()
 
     def run(self):
-        [m() for m in self.models]
+        self.apply_input_tables(tables=self.input_tables, to=self.models, when=self.time)
+
+        # Update environment boundary conditions
+        self.soil()
+
+        # Update topological surfaces and volumes based on other evolved structural properties
+        self.root_anatomy()
+        
+        # Compute state variations for water and then carbon and nitrogen
+        self.root_water()
+        self.root_nitrogen()
+
+        self.time += 1
